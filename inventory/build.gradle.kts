251e6c59876d7e4c34b8178a90fe860a39f49e53
K4NEI <kimyoungho0415@gmail.com>
2026-05-08 19:47:34 +0900

    Split modules [inventory-service inventory inventory-event] to ktcloud-msa-inventory-service

---
+++ inventory/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.jpa")
+}
+
+dependencies {
+    implementation("https://maven.pkg.github.com/kanei0415/ktcloud-market-msa-common:1.0.0")
+    implementation("https://maven.pkg.github.com/kanei0415/ktcloud-market-msa-client-redis:1.0.0")
+    implementation(project(":inventory-event"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    implementation("org.springframework.kafka:spring-kafka")
+
+    runtimeOnly("com.h2database:h2")
+}
